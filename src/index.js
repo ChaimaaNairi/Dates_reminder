684fecee4b58f302de04f1596eab25af0a791da1
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-05 13:24:44 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { ReactDOM } from 'react-dom'
+import ReactDOM from 'react-dom';
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
+ReactDOM.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
