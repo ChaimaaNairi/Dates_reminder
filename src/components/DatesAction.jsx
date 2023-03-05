d2bc29f17dc9bb58ef9e7e85534bfa743b3c17b1
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-05 14:07:13 +0300

    commit

---
+++ src/components/DatesAction.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+const DatesAction = ({deleteData,onViewData}) => {
+    return (
+        <Row className="justify-content-center my-2 ">
+          <Col sm="8" className="d-flex justify-content-between">
+            <button onClick={deleteData} className="btn-style p-2">Delete all </button>
+            <button onClick={onViewData} className="btn-style p-2">Show data</button>
+          </Col>
+        </Row>
+    )
+}
+
+export default DatesAction
---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const person = [{ id: '0', name: 'Hasan', date: 'evening 5 ', img: 'p1.jpg' },
+{ id: '1', name: 'Ahmet', date: 'evening 4 ', img: 'p1.jpg' },
+{ id: '2', name: 'Esra', date: 'evening 3 ', img: 'p1.jpg' },
+{ id: '3', name: 'Meryem', date: 'evening 2 ', img: 'p1.jpg' },
+{ id: '4', name: 'Ayshe', date: 'evening 1 ', img: 'p1.jpg' },
+{ id: '5', name: 'Hasan', date: 'evening 1 ', img: 'p1.jpg' },
+{ id: '6', name: 'Esra', date: 'evening 1 ', img: 'p1.jpg' },]
